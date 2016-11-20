7f9a85ea27064bbb9f81286888be8f1c2769bf04
Anthony Charles <anthonycharles@gmail.com>
2016-11-20 17:38:00 +0100

    Adding likes/dislikes to the detail page

--- src/main/java/fr/hei/devweb/cityexplorer/servlets/OpinionServlet.java
+++ src/main/java/fr/hei/devweb/cityexplorer/servlets/OpinionServlet.java
@@ -15,25 +15,32 @@ public class OpinionServlet extends AbstractGenericServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String idCityLike = req.getParameter("like");
-		if(idCityLike != null) {
-			try {
+		String idCityDislike = req.getParameter("dislike");
+
+		try {
+			if (idCityLike != null) {
 				Integer cityId = Integer.parseInt(idCityLike);
 				CityService.getInstance().likeCity(cityId);
-			} catch (NumberFormatException e) {
-			}
-		}
-		
-		String idCityDislike = req.getParameter("dislike");
-		if(idCityDislike != null) {
-			try {
+				this.redirectToPage(cityId, req, resp);
+			} else if (idCityDislike != null) {
 				Integer cityId = Integer.parseInt(idCityDislike);
 				CityService.getInstance().dislikeCity(cityId);
-			} catch (NumberFormatException e) {
+				this.redirectToPage(cityId, req, resp);
+			} else {
+				resp.sendRedirect("home");
 			}
+		} catch (NumberFormatException e) {
+			resp.sendRedirect("home");
 		}
-		
-		resp.sendRedirect("home");
 	}
 
+	private void redirectToPage(Integer cityId, HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		String returnPage = req.getParameter("return");
+		if("detail".equals(returnPage)) {
+			resp.sendRedirect(String.format("detail?id=%d", cityId));
+		} else {
+			resp.sendRedirect("home");
+		}
+	}
 	
 }
